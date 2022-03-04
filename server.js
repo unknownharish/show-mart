17bbc750b8c2f6cdcacf908e7487c8d9e6674d1f
harish <chandharish198@gmail.com>
2022-03-04 08:27:46 +0530

    final

--- server.js
+++ server.js
@@ -14,10 +14,10 @@ if (process.env.NODE_ENV == 'production') {
 
     app.use(express.static(path.join(__dirname, '/client/build')));
 
-    // app.get('*', (req, res) => {
+    app.get('*', (req, res) => {
 
-    //     res.sendFile(path.join(__dirname, 'client ', 'build', 'index.html'));
-    // })
+        res.sendFile(path.join(__dirname, 'client ', 'build', 'index.html'));
+    })
 } else {
 
     app.get('/', (req, res) => {
